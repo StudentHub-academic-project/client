e1dc878963d22671d7b854ea96ff72b6e8105fa6
anastasia-sl <anastasiaslynko2206@gmail.com>
2024-05-24 13:44:04 +0300

    Login page in process, registration page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import RegistrationPage from "./pages/RegistrationPage";
+import LoginForm from "./molecules/LoginForm";
 
 function App() {
   return (
     <div className="App">
-      <RegistrationPage />
+      {/*<RegistrationPage />*/}
+        <LoginForm />
     </div>
   );
 }
---
+++ my-app/src/pages/LoginPage/index.js
@@ -0,0 +1,12 @@
+import './style.scss';
+import LoginForm from "../../molecules/LoginForm";
+
+function LogInPage() {
+    return (
+        <div className='LoginPageDiv'>
+            <LoginForm/>
+        </div>
+    );
+}
+
+export default LogInPage;
